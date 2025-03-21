a29ad33a8f79d971b1b4f5fd5f93e1d4563845cc
Abhrajit Chatterjee <abhrajitc23@gmail.com>
2025-03-21 12:12:14 +0530

    City Service Implemented

---
+++ src/services/city_service.js
@@ -0,0 +1,52 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity({ name }) {
+        try {
+            const city = await this.cityRepository.createCity({ name });
+            return city;
+        } catch (error) {
+            console.error("Something went wrong in the city service layer");
+            throw { error };
+        }
+    }
+
+    async deleteCity(cityid) {
+        try {
+            const response = await this.cityRepository.deleteCity(cityid);
+            return response;
+        }
+        catch (error) {
+            console.error("Something went wrong in the city service layer");
+            throw { error };
+        }
+    }
+
+
+    async updateCity(cityid, data) {
+        try {
+            const city = await this.cityRepository.updateCity(cityid, data);
+            return city;
+        }
+        catch (error) {
+            console.error("Something went wrong in the city service layer");
+            throw { error };
+        }
+    }
+
+    async getCity(cityid) {
+        try {
+            const city = await this.cityRepository.getCity(cityid);
+            return city;
+        }
+        catch (error) {
+            console.error("Something went wrong in the city service layer");
+            throw { error };
+        }
+    }
+}
+module.exports = CityService;
